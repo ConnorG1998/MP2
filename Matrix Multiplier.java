995e81b9490405dc4a306eb0115a6bd90b96e8b6
ConnorG1998 <82125974+ConnorG1998@users.noreply.github.com>
2021-10-12 09:47:20 -0500

    MP2 Created

---
+++ Matrix Multiplier.java
@@ -0,0 +1,17 @@
+//Machine Problem 2 Group 10
+//Alfonso Gonzalez & Connor Griffith
+//9/23/2021
+import java.util.Scanner;
+public class Calculator {
+    private String name;
+
+    public ;
+
+    private void () {
+       
+
+    public static void main() {
+        
+        }
+    }
+}
