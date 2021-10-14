1174c290c3b87664c36dc4639ed5bb462dee8ec4
Shade-Zepheri <ziroalpha@gmail.com>
2021-10-13 22:04:01 -0500

    Finish matrix generation

--- Calculator.java
+++ Calculator.java
@@ -8,15 +8,13 @@ import java.util.List;
 import java.util.Random;
 
 public class Calculator {
-    private ArrayList<ArrayList<Float>> matrix1;
-    private ArrayList<ArrayList<Float>> matrix2;
+    private List<List<Integer>> matrix1;
+    private List<List<Integer>> matrix2;
 
     private int threadCount = 5;
 
     public Calculator() {
         // SImply call to generate matrix
-        this.matrix1 = new ArrayList<>();
-        this.matrix2 = new ArrayList<>();
         generateMatrices();
     }
 
@@ -24,43 +22,58 @@ public class Calculator {
 
     private void generateMatrices() {
         // Populate the 20x20 matrix
-        // Temporary for testing
-        
-        Random rand;
-        int randomNum = rand.nextInt(101);
+        Random rand = new Random();
         int rows = 20;
         int columns = 20;
-        int[][] array = new int[rows][columns];
 
-        for(int i = 0; i<rows; i++)
-            for(int j = 0; j<columns; j++)
-                array[i][j] = randomNum; //make each index a random integer between zero and 1
-        for(int i = 0; i<rows; i++)
-        {
-            for(int j = 0; j<columns; j++) {
-        System.out.print(array[i][j]);
+        // Create matrix 1
+        System.out.println("Creating Matrix 1");
+        
+        matrix1 = new ArrayList<>();
+        for (int i = 0; i < rows; i++) {
+            // Populate our row
+            ArrayList<Integer> row = new ArrayList<>();
+            for (int j = 0; j < columns; j++) {
+                int n = rand.nextInt(10);
+                System.out.print(n);
+                System.out.print(", ");
+
+                row.add(n);
+            }
+            
+            matrix1.add(row);
+            System.out.println("");
         }
-        System.out.println();
-        }
-        for (int i = 0; i < 20; i++) {
-            ArrayList<Float> row = new ArrayList<>();
-            for (int j = 0; j < 20; j++) {
-                row.add(Float.valueOf(j));
+        
+        // Create matrix 2
+        System.out.println("Creating Matrix 1");
+        
+        matrix2 = new ArrayList<>();
+        for (int i = 0; i < rows; i++) {
+            // Populate our row
+            ArrayList<Integer> row = new ArrayList<>();
+            for (int j = 0; j < columns; j++) {
+                int n = rand.nextInt(10);
+                System.out.print(n);
+                System.out.print(", ");
+
+                row.add(n);
             }
 
             matrix2.add(row);
+            System.out.println("");
         }
     }
 
     // Matrix multiplication
 
     private class Runner implements Runnable {
-        private List<ArrayList<Float>> rows;
-        private List<ArrayList<Float>> cols;
+        private List<List<Integer>> rows;
+        private List<List<Integer>> cols;
 
-        private Float result;
+        private Integer result;
 
-        public Runner(List<ArrayList<Float>> rows, List<ArrayList<Float>> cols) {
+        public Runner(List<List<Integer>> rows, List<List<Integer>> cols) {
             this.rows = rows;
             this.cols = cols;
         }
@@ -81,26 +94,26 @@ public class Calculator {
 
         // Results
 
-        public Float getResult() {
+        public Integer getResult() {
             // Simply retrieve our result
             return result;
         }
     }
 
-    public ArrayList<ArrayList<Float>> colsFromSubIndicies(int bottomIndex, int topIndex) {
+    public List<List<Integer>> colsFromSubIndicies(int bottomIndex, int topIndex) {
         // Since top level arraylist is rows, and sublists contain column entries, we need to iterate through the nth entries of the rows and add those entries
         // Create our cols
-        ArrayList<ArrayList<Float>> cols = new ArrayList<>();
+        List<List<Integer>> cols = new ArrayList<>();
         for (int i = 0; i < 4; i++) {
             cols.add(new ArrayList<>());
         }
 
         // Retrieve from matrix
         for (int i = 0; i < matrix2.size(); i++) {
-            ArrayList<Float> row = matrix2.get(i);
+            List<Integer> row = matrix2.get(i);
             
             for (int j = bottomIndex; j < topIndex; j++) {
-                Float value = row.get(j);
+                Integer value = row.get(j);
                 cols.get(j - bottomIndex).add(value);
             }
         }
@@ -108,15 +121,15 @@ public class Calculator {
         return cols;
     }
 
-    public Float matrixProduct() {
+    public Integer matrixProduct() {
         // We create our threads
         List<Thread> threads = new ArrayList<>();
         for (int i = 0; i < threadCount; i++) {
             // Get subarrays
             int bottomIndex = i * 4;
             int topIndex = (i + 1) * 4;
-            List<ArrayList<Float>> rows = matrix1.subList(bottomIndex, topIndex);
-            ArrayList<ArrayList<Float>> cols = colsFromSubIndicies(bottomIndex, topIndex);
+            List<List<Integer>> rows = matrix1.subList(bottomIndex, topIndex);
+            List<List<Integer>> cols = colsFromSubIndicies(bottomIndex, topIndex);
             
             // Create our thread from a portion of the matricies
             Runner runner = new Runner(rows, cols);
@@ -124,12 +137,12 @@ public class Calculator {
         }
 
         // Temporary to get compiling
-        return Float.valueOf(0);
+        return 0;
     }
 
     public static void main(String[] args) {
         Calculator calc = new Calculator();
-        System.out.println();
+
         // Perform calculations
     }
 }
